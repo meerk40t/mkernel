fb156aaa7c7a420994d5c474f97f77e06cd78471
tatarize <tatarize@gmail.com>
2023-12-17 03:33:05 -0800

    Update tests for new lifecycle method

--- test/bootstrap.py
+++ test/bootstrap.py
@@ -2,7 +2,7 @@ from kernel import Kernel
 
 
 def bootstrap(profile="TestingDevice"):
-    kernel = Kernel("TestDevice", version="0", profile="TestDevice")
-    kernel()
+    kernel = Kernel("TestDevice", version="0", profile="TestDevice", ignore_settings=True)
+    kernel(partial=True)
     kernel.console("channel print console\n")
     return kernel
